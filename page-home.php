e671f055953c60ac138bbdfcb8e59310fb348ee8
Chris Bishop <domingobishop@gmail.com>
2017-11-13 12:06:31 +0000

    homepage template

---
+++ page-home.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Template Name: Latin home
+ *
+ */
+get_header(); ?>
+
+<?php get_template_part( 'breadcrumb' ); ?>
+
+<div id="primary" class="content-area latin-home">
+	<div class="container">
+		<div class="row">
+			<main id="main" class="col-xs-12 col-sm-8 col-md-8" role="main">
+				<?php
+				while ( have_posts() ) : the_post();
+					get_template_part( 'content' );
+				endwhile;
+				?>
+			</main>
+			<?php
+			$sidebar = get_post_meta( $post->ID, 'sidebar', true );
+			if ( $sidebar == 'false' ) {
+				// do nothing
+			} else {
+				get_sidebar( $sidebar );
+			}
+			?>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
