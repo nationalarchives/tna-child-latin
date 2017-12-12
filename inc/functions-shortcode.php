bc0a1332496c878ea33901226406e3769dcf1d94
Chris Bishop <domingobishop@gmail.com>
2017-12-12 13:53:31 +0000

    activity 2

--- inc/functions-shortcode.php
+++ inc/functions-shortcode.php
@@ -13,6 +13,9 @@ function latin_activity_shortcode( $atts ) {
 		case '1':
 			return activity_1();
 			break;
+		case '2':
+			return activity_2();
+			break;
 	}
 }
 add_shortcode( 'latin', 'latin_activity_shortcode' );
@@ -20,10 +23,21 @@ add_shortcode( 'latin', 'latin_activity_shortcode' );
 function activity_1() {
 	?>
 	<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" width="525" height="415" id="theMovie">
-		<param name="src" value="/activities/activity01.swf">
+		<param name="src" value="<?php echo get_stylesheet_directory_uri() ?>/activities/activity01.swf">
 		<param name="menu" value="false">
 		<param name="wmode" value="transparent">
 		<embed src="/latin/beginners/swf/activity01.swf" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" width="525" height="415" menu="false" wmode="transparent" name="theMovie">
 	</object>
 <?php
+}
+
+function activity_2() {
+	?>
+	<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" width="525" height="415" id="theMovie">
+		<param name="src" value="<?php echo get_stylesheet_directory_uri() ?>/activities/activity02.swf">
+		<param name="menu" value="false">
+		<param name="wmode" value="transparent">
+		<embed src="/latin/beginners/swf/activity01.swf" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" width="525" height="415" menu="false" wmode="transparent" name="theMovie">
+	</object>
+	<?php
 }
