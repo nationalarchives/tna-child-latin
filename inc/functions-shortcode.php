0f217b758fff2345783c8432330cbfca5318b468
Chris Bishop <domingobishop@gmail.com>
2017-12-12 10:36:50 +0000

    activity_1

--- inc/functions-shortcode.php
+++ inc/functions-shortcode.php
@@ -2,3 +2,14 @@
 /**
  * Shortcode
  */
+
+function activity_1() {
+	?>
+	<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" width="525" height="415" id="theMovie">
+		<param name="src" value="/activities/activity01.swf">
+		<param name="menu" value="false">
+		<param name="wmode" value="transparent">
+		<embed src="/latin/beginners/swf/activity01.swf" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" width="525" height="415" menu="false" wmode="transparent" name="theMovie">
+	</object>
+<?php
+}
