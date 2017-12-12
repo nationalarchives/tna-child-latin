01484c7ef2d00d02bed181c163ee6b30a56ab448
Chris Bishop <domingobishop@gmail.com>
2017-12-12 16:14:53 +0000

    dynamic resize embed

--- inc/functions-shortcode.php
+++ inc/functions-shortcode.php
@@ -22,11 +22,12 @@ add_shortcode( 'latin', 'latin_activity_shortcode' );
 
 function activity_1() {
 	?>
-	<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" width="525" height="415" id="theMovie">
+	<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0" width="100%" height="100%" id="theMovie">
 		<param name="src" value="<?php echo get_stylesheet_directory_uri() ?>/activities/activity01.swf">
 		<param name="menu" value="false">
 		<param name="wmode" value="transparent">
-		<embed src="/latin/beginners/swf/activity01.swf" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" width="525" height="415" menu="false" wmode="transparent" name="theMovie">
+		<PARAM NAME="SCALE" VALUE="exactfit">
+		<embed src="<?php echo get_stylesheet_directory_uri() ?>/activities/activity01.swf" SCALE="exactfit" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" width="100%" height="100%" menu="false" wmode="transparent" name="theMovie">
 	</object>
 <?php
 }
@@ -37,7 +38,7 @@ function activity_2() {
 		<param name="src" value="<?php echo get_stylesheet_directory_uri() ?>/activities/activity02.swf">
 		<param name="menu" value="false">
 		<param name="wmode" value="transparent">
-		<embed src="/latin/beginners/swf/activity01.swf" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" width="525" height="415" menu="false" wmode="transparent" name="theMovie">
+		<embed src="<?php echo get_stylesheet_directory_uri() ?>/activities/activity02.swf" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" width="525" height="415" menu="false" wmode="transparent" name="theMovie">
 	</object>
 	<?php
 }
