55ecce01bfdcdb191f80e2862f8f29254149b7c2
AshTNA <ashleyaburnett@gmail.com>
2018-01-16 13:56:10 +0000

    Add condition for metaboxes to only load where relevant

--- inc/functions-admin.php
+++ inc/functions-admin.php
@@ -1,6 +1,20 @@
 <?php
 
 function latin_metaboxes() {
+
+	if (isset($_GET['post'])) {
+		$post_id = $_GET['post'];
+	} else {
+		if (isset($_POST['post_ID'])) {
+			$post_id = $_POST['post_ID'];
+		} else {
+			$post_id = '';
+		}
+	}
+	if( !isset( $post_id ) ) return;
+
+
+
 	$latin_metaboxes = array(
 		array(
 			'id' => 'page_options',
@@ -19,7 +33,13 @@ function latin_metaboxes() {
 			)
 		)
 	);
-	foreach ( $latin_metaboxes as $meta_box ) {
-		$notification_box = new CreateMetaBox( $meta_box );
+
+	$template_file = get_post_meta($post_id, '_wp_page_template', true);
+
+	if( $template_file == 'page-home.php' ) {
+		foreach ( $latin_metaboxes as $meta_box ) {
+			$notification_box = new CreateMetaBox( $meta_box );
+		}
 	}
+
 }
