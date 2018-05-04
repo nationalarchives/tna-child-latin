aa23f77102eecc59e838703602b14f722b048238
AshTNA <ashleyaburnett@gmail.com>
2018-05-04 11:24:27 +0100

    Change precrumbs/prepath to reflex proper link structure

--- inc/functions-globals.php
+++ inc/functions-globals.php
@@ -5,29 +5,29 @@
  *
  * @param string $environment - a string of either 'local', 'development' or 'external'
  */
-function setThemeGlobals($environment = null) {
-    if ($environment === null) {
-        throw new BadFunctionCallException('setThemeGlobals function must be passed at string that represents the environment');
-    }
+function setThemeGlobals( $environment = null ) {
+	if ( $environment === null ) {
+		throw new BadFunctionCallException( 'setThemeGlobals function must be passed at string that represents the environment' );
+	}
 
-    global $pre_path;
-    global $pre_crumbs;
+	global $pre_path;
+	global $pre_crumbs;
 
-    switch ($environment) {
-        case 'internal':
-        case 'development':
-            $pre_path = '';
-            $pre_crumbs = array('Help with your research'         => '/help-with-your-research/',
-                                'Reading old documents'           => '/help-with-your-research/reading-old-documents/',
-                                'Learn medieval Latin' => '/');
-            break;
-        case 'external':
-            $pre_crumbs = array(
-	            'Help with your research'         => '/help-with-your-research/',
-	            'Reading old documents'           => '/help-with-your-research/reading-old-documents/',
-                'Learn medieval Latin' => '/latin/'
-            );
-            $pre_path = '/latin';
-            break;
-    }
+	switch ( $environment ) {
+		case 'internal':
+		case 'development':
+			$pre_path   = '';
+			$pre_crumbs = array(
+				'Learn medieval Latin'    => '/'
+			);
+			break;
+		case 'external':
+			$pre_crumbs = array(
+				'Help with your research' => '/help-with-your-research/',
+				'Reading old documents'   => '/help-with-your-research/reading-old-documents/',
+				'Learn medieval Latin'    => '/help-with-your-research/reading-old-documents/latin/'
+			);
+			$pre_path   = '/help-with-your-research/reading-old-documents/latin';
+			break;
+	}
 }
