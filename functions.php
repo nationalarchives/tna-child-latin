8c4014249e2d00f6481052a836ca517c6b872951
netdesignr <michaiel.webdesigner@gmail.com>
2021-03-29 14:44:35 +0100

    Create cookie test script

--- functions.php
+++ functions.php
@@ -18,3 +18,55 @@ add_action('wp_head', 'dequeue_parent_style', 9999);
 add_action('wp_enqueue_scripts', 'tna_child_styles');
 add_action('wp_enqueue_scripts','tna_child_scripts');
 add_action( 'init', 'latin_metaboxes' );
+
+class Manage_Acceptable_Group_Cookie_List implements JsonSerializable {
+    protected $usage;
+    protected $settings;
+    protected $essential;
+    public function __construct(array $cookie_options)
+    {
+        $this->usage        = $cookie_options[‘usage’];
+        $this->settings     = $cookie_options[‘settings’];
+        $this->essential    = $cookie_options[‘essential’];
+    }
+    public function getEssential()
+    {
+        return $this->essential;
+    }
+    public function getSettings()
+    {
+        return $this->settings;
+    }
+    public function getUsage()
+    {
+        return $this->usage;
+    }
+    public function jsonSerialize()
+    {
+        return
+        [
+            ‘usage’       => $this->getUsage(),
+            ‘settings’    => $this->getSettings(),
+            ‘essential’   => $this->getEssential()
+        ];
+    }
+}
+$manage_acceptable_group_cookie_list = new Manage_Acceptable_Group_Cookie_List(
+    array(
+        ‘usage’         => false,
+        ‘settings’      => false,
+        ‘essential’     => true,
+    )
+);
+$siteUrl = site_url();
+if (strpos($siteUrl, ‘latin’) !== false) {
+    if(!isset($_COOKIE[‘cookies_policy’])) {
+        $cookie_group_list_to_json = json_encode( $manage_acceptable_group_cookie_list );
+        setcookie(‘cookies_policy’, $cookie_group_list_to_json, time()+31556926  ,‘/’);
+        /**
+        * Call $_COOKIE directly after was set so it can be accessed
+        * without having to reload or navigate to a different page
+        */
+        $_COOKIE[‘cookies_policy’] = $cookie_group_list_to_json;
+    }
+}
