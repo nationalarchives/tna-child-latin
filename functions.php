908d3c8cddd6122ddd0958e4be9d5b00e53bb9e2
netdesignr <michaiel.webdesigner@gmail.com>
2021-04-07 13:21:50 +0100

    Disable WP Super Cache

--- functions.php
+++ functions.php
@@ -18,3 +18,6 @@ add_action('wp_head', 'dequeue_parent_style', 9999);
 add_action('wp_enqueue_scripts', 'tna_child_styles');
 add_action('wp_enqueue_scripts','tna_child_scripts');
 add_action( 'init', 'latin_metaboxes' );
+
+// Disable WP Super Cache
+define('DONOTCACHEPAGE', true);
